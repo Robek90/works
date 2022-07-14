8c4f32d9f20e924b12b73d3fb2b9f6068f6e027a
Robek90 <tazz90@mail.ru>
2022-07-14 16:09:23 +0300

    create main page and carousel

---
+++ prosper_portfolio/slider/slider.js
@@ -0,0 +1,82 @@
+const slideContainer = document.querySelector('.slider');
+const sliderText = document.querySelector('.slider--text');
+const btnLeft = document.querySelector('.slider__btn-left');
+const btnRight = document.querySelector('.slider__btn-right');
+
+const sliderImages = [
+  {
+    src: './images/main_carousel/2.jpg',
+  },
+  {
+    src: './images/main_carousel/3.jpg',
+  },
+  {
+    src: './images/main_carousel/4.jpg',
+  },
+  {
+    src: './images/main_carousel/5.jpg',
+  },
+];
+
+let slideCounter = 0;
+
+const startSlider = () => {
+  slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+    ), url(${sliderImages[0].src})`;
+};
+
+btnRight.addEventListener('click', function() {
+  if (slideCounter === sliderImages.length - 1) {
+    slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+    ), url(${sliderImages[0].src})`;
+    slideCounter = -1;
+
+    slideContainer.classList.add('fadeIn');
+    setTimeout(() => {
+      slideContainer.classList.remove('fadeIn');
+    }, 1000);
+  }
+  slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+      ),url(${sliderImages[slideCounter + 1].src})`;
+  slideCounter++;
+  slideContainer.classList.add('fadeIn');
+  setTimeout(() => {
+    slideContainer.classList.remove('fadeIn');
+  }, 1000);
+});
+
+btnLeft.addEventListener('click', function() {
+  if (slideCounter === 0) {
+    slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+    ),url(${sliderImages[sliderImages.length - 1].src})`;
+    slideCounter = sliderImages.length;
+    slideContainer.classList.add('fadeIn');
+    setTimeout(() => {
+      slideContainer.classList.remove('fadeIn');
+    }, 1000);
+  }
+
+  slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+    ),url(${sliderImages[slideCounter - 1].src})`;
+  slideCounter--;
+  slideContainer.classList.add('fadeIn');
+  setTimeout(() => {
+    slideContainer.classList.remove('fadeIn');
+  }, 1000);
+});
+document.addEventListener('DOMContentLoaded', startSlider);
