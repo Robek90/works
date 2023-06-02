16b3abc0d51c98e4b0d109b472e4a46434be1ad8
Robek90 <tazz90@mail.ru>
2023-06-02 21:27:13 +0300

    add multi active figure

--- wh_project_react/client/src/pages/galaxy/scene/animation/index.js
+++ wh_project_react/client/src/pages/galaxy/scene/animation/index.js
@@ -8,34 +8,35 @@ function Animation(props) {
   const [clicked, click] = useState(false);
 
   useFrame((state, delta) => {
-    ref.current.position.x = Math.random() * 2;
-    ref.current.position.y = Math.random() * 2;
-    ref.current.position.z = Math.random() * 2;
-    ref.current.scale.setScalar(Math.random() * 0.05);
-    ref.current.rotation.y -= 0.001; 
-    ref.current.rotation.x += 0.0001;
-    ref.current.rotation.z += 0.0001;
+    // ref.current.position.x = Math.random() * 2;
+    // ref.current.position.y = Math.random() * 2;
+    // ref.current.position.z = Math.random() * 2;
+    // ref.current.scale.setScalar(Math.random() * 0.05);
+    ref.current.rotation.y -= 0.01; 
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.z += 0.01;
   });
 
   return (
-    <>
+    <group>
       <mesh
         {...props}
         ref={ref}
-        scale={clicked ? 1.5 : 1}
+        scale={clicked ? 20 : 1}
         onClick={(event) => click(!clicked)}
         onPointerOver={(event) => hover(true)}
         onPointerOut={(event) => hover(false)}
       >
-        <tetrahedronGeometry args={[1]} />
+        <tetrahedronGeometry args={props.data.random}/>
         <meshStandardMaterial
           flatShading={true}
           wireframe={props.wireframe}
           color={hovered ? "hotpink" : "orange"}
         />
       </mesh>
-    </>
+    </group>
   );
 }
  
 export default Animation;
+
--- wh_project_react/client/src/pages/galaxy/scene/index.js
+++ wh_project_react/client/src/pages/galaxy/scene/index.js
@@ -1,99 +1,23 @@
-// import React, { Component } from 'react'
-// import * as THREE from 'three'
-
-// class Scene extends Component {
-//   constructor(props) {
-//     super(props)
-
-//     this.start = this.start.bind(this)
-//     this.stop = this.stop.bind(this)
-//     this.animate = this.animate.bind(this)
-//   }
-
-//   componentDidMount() {
-//     const width = this.mount.clientWidth
-//     const height = this.mount.clientHeight
-
-//     const scene = new THREE.Scene()
-//     const camera = new THREE.PerspectiveCamera(
-//       30,
-//       width / height,
-//       0.1,
-//       1000
-//     )
-//     const renderer = new THREE.WebGLRenderer({ antialias: true })
-//     const geometry = new THREE.BoxGeometry(1, 1, 1)
-//     const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
-//     const cube = new THREE.Mesh(geometry, material)
-
-//     camera.position.z = 4
-//     scene.add(cube)
-//     renderer.setClearColor('#000000')
-//     renderer.setSize(width, height)
- 
-//     this.scene = scene
-//     this.camera = camera
-//     this.renderer = renderer
-//     this.material = material
-//     this.cube = cube
-
-//     this.mount.appendChild(this.renderer.domElement)
-//     this.start()
-//     console.log(this)
-//   }
-
-//   componentWillUnmount() {
-//     this.stop()
-//     this.mount.removeChild(this.renderer.domElement)
-//   }
-
-//   start() {
-//     if (!this.frameId) {
-//       this.frameId = requestAnimationFrame(this.animate)
-//     }
-//   }
-
-//   stop() {
-//     cancelAnimationFrame(this.frameId)
-//   }
-
-//   animate() {
-//     this.cube.rotation.x += 0.01
-//     this.cube.rotation.y += 0.01
-
-//     this.renderScene()
-//     this.frameId = window.requestAnimationFrame(this.animate)
-//   }
-
-//   renderScene() {
-//     this.renderer.render(this.scene, this.camera)
-//   }
-
-//   render() {
-//     return (
-//       <div
-//         style={{ width: '400px', height: '400px' }}
-//         ref={(mount) => { this.mount = mount }}
-//       />
-//     )
-//   }
-// }
-
-// export default Scene
-
 import { Canvas } from "@react-three/fiber";
-import Cylinder3d from "./animation/index";
+import Galaxy from "./animation/index";
+import { OrbitControls, Stats } from "@react-three/drei";
 import "./style.css";
  
 function Scene() {
+  const randomVector = (r) => [r/2 - Math.random() * r, r/2 - Math.random() * r, r/2 - Math.random() * r]
+  const randomEuler = () => [Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI]
+  const data = Array.from({ length: 1000 }, (r = 500) => ({ random: Math.random(), position: randomVector(r), rotation: randomEuler() }))
+
   return (
     <>
       <section className="scene">
-        {/* Canvas 1 */}
         <Canvas>
-          <pointLight position={[10, 10, 10]} />
+          <pointLight position={[30, 30, 30]} />
           <ambientLight />
-          <Cylinder3d/>
+          {data.map(item=>{
+            return <Galaxy position={item.position} data={data}/>
+          })}
+          <OrbitControls/>
         </Canvas>
       </section>
     </>
